f84a6d88561390b7a93416c0435fc1906d4a6004
Neosake <70122910+Neosake@users.noreply.github.com>
2022-10-12 20:38:34 -0500

    Create main.purs

---
+++ src/main.purs
@@ -0,0 +1,13 @@
+{- Import Required Modules. -}
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude
+
+{- Define Needed Functions. -}
+main :: Effect Unit
+
+{- Implement The Binding's. -}
+
+{- Implement Main Function. -}
+main = do
+  log("Satisfies any possible errors.")
