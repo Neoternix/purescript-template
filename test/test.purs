f396ab5e1dffddf9d2710bddceacfae5bbc8c3f3
Neosake <70122910+Neosake@users.noreply.github.com>
2022-10-12 20:39:54 -0500

    Create test.purs

---
+++ test/test.purs
@@ -0,0 +1,14 @@
+{- Import Required Modules. -}
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude
+
+{- Define Needed Functions. -}
+main :: Effect Unit
+
+{- Implement The Binding's. -}
+test = main
+
+{- Implement Main Function. -}
+test = do
+  log("Satisfies any possible errors.")
